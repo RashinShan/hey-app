0a4f589d455238398e0d5247525e4e67af939da1
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-08-04 16:48:30 +0530

    Add files via upload
    my new

---
+++ ex3.php
@@ -0,0 +1,16 @@
+<?php
+    $num=10;
+
+    if($hour>10)
+    {
+        echo "Have a warm day!";
+    }
+    else if($num==10)
+    {
+        echo "good day";
+    }
+    else
+    {
+        echo "Good Night";
+    }
+?>
