6fb3c60370915131feef4aec29f8d305d140aef0
PramodSithara <67549834+PramodSithara@users.noreply.github.com>
2023-08-16 12:36:16 +0530

    Add files via upload
    create models
    log_in.html (Back end)

--- hayapp/chatapplycation_heyapp/heyapp/urls.py
+++ hayapp/chatapplycation_heyapp/heyapp/urls.py
@@ -7,11 +7,11 @@ from django.conf.urls.static import static
 urlpatterns = [
 
 
-    path('',views.homefunc,name='home' ),
+    path('',views.reg,name='sign_up' ),
     path('home/',views.homefunc,name='home' ),
     path('chat/',views.chatfunc,name='chat' ),
     path('login/',views.loginfunc,name='login' ),
     path('signup_OTP/',views.signup_OTPfunc,name='signup_OTP' ),
-    path('signup/',views.signupfunc,name='signup' ),
     path('userlist/',views.userlistfunc,name='userlist' ),
+    path('register/',views.register,name='register' ),
 ]
--- hayapp/chatapplycation_heyapp/heyapp/views.py
+++ hayapp/chatapplycation_heyapp/heyapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User, Message
 
 
 def chatfunc (request):
     return render(request,'chat.html')
 
+
 def homefunc (request):
      return render(request,'home.html')
+
+def reg(request):
+     return render(request, 'signup.html')
+
 
 def loginfunc (request):
      return render(request,'login.html')
@@ -14,9 +20,20 @@ def loginfunc (request):
 def signup_OTPfunc (request):
      return render(request,'signup_OTP.html')
 
-
-def signupfunc (request):
-     return render(request,'signup.html')
-
 def userlistfunc (request):
      return render(request,'userlist.html')
+
+def register(request):
+     firstname = request.POST['firstname']
+     lastname = request.POST['lastname']
+     dob = request.POST['dob']
+     email = request.POST['email']
+     username = request.POST['uname']
+     password = request.POST['password']
+
+
+
+     new_user = User.objects.create(firstname=firstname, lastname=lastname, dob=dob, email=email, username=username, password=password)
+     new_user.save()
+     return HttpResponse('Data enter Successfully')
+
